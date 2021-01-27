de4b4beaed559cf168858c1846837dd0af233b79
lilkeza <70450530+lilkeza@users.noreply.github.com>
2021-01-27 22:55:24 +0100

    Finished todolist!
    First javascript project using the console in browser.

---
+++ todolist.js
@@ -0,0 +1,27 @@
+const todolist = []
+let input = prompt("What would you like to do?");
+while (input !== "quit" && input !== "q") {
+    if (input === "new") {
+        const newtodo = prompt("Enter a new todo!");
+        console.log(`${newtodo} added to the list`);
+        todolist.push(newtodo)
+    } else if (input === "list") {
+        console.log("**********")
+        for (let i = 0; i < todolist.length; i++) {
+            console.log(i + ': ' + todolist[i]);
+        }
+        console.log("**********")
+    } else if (input === "delete") {
+        const remove = parseInt(prompt("Enter index to remove"));
+        if (!Number.isNaN(remove)) {
+            const deleted = todolist.splice(remove, 1);
+            console.log(`Ok, deleted todo: ${deleted[0]}`)
+        } else {
+            console.log("Unknown index")
+        }
+    } else {
+        console.log("Please enter a valid command!")
+    }
+    input = prompt("What would you like to do?")
+}
+console.log("Ok Goodbye!")
